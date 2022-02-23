90f0b9197365324b11d8f2a96e128e67c7bb1397
anupamck <anupam.ck@gmailcom>
2022-02-23 21:42:50 +0100

    strengthen hit check

--- battleship.js
+++ battleship.js
@@ -4,34 +4,43 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-var cell1 = getRandomInt(1, 5);
-var cell2 = cell1 + 1;
-var cell3 = cell1 + 2;
+function setupBoard(boardLength, shipLength) {
+  var board = new Array(boardLength).fill("w");
+  var shipStart = getRandomInt(0, board.length - shipLength)
+  for (i = shipStart; i <= shipStart + 2; i++) {
+    board[i] = "s";
+    }
+    return board;
+  }
+
+board = setupBoard(7, 3);
+console.log(board);
+
 var sunk = false;
 var hits = 0;
 
 while(!sunk) {
-  guess = prompt("Enter a cell to shoot at (1-7)");
+  guess = parseInt(prompt("Enter a cell to shoot at (1-7)") - 1);
 
-  if(Number.isInteger(parseInt(guess)) && (guess >= 1) && (guess <=7))
+  if(Number.isInteger(guess) && (guess >= 0) && (guess <= 6))
   {
-  if((guess == cell1) || (guess == cell2) || (guess == cell3)){
+  if(board[guess] == "s") {
     hits++;
+    board[guess] = "w";
     alert("Ouch! That hurt");
   }
   else {
     alert("Haw haw, you missed!");
   }
 
+  console.log(board);
+
   if(hits == 3) {
     sunk = true;
     alert("You have sunk my ship!")
   }
-
-  console.log("hits = " + hits);
 }
 else {
     alert("I don't have all day. Do it right this time.");
-    console.log(Number.isInteger(parseInt(guess)));
 }
 }
